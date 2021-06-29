4bd06b96e5bf41f2899d107eff8bc82dde523558
Barta Valentin <bartavalentin99@gmail.com>
2021-06-29 11:22:48 +0200

    remove storage table

--- Second/HttpTrigger1/index.ts
+++ Second/HttpTrigger1/index.ts
@@ -7,18 +7,6 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
-    const value: string = process.env['MY_FIRST_VALUE'];
-
-    context.bindings.tableBindings = [];
-
-    context.bindings.tableBindings.push({
-        PartitionKey: 'Test',
-        RowKey: value,
-        name
-    });
-
-    console.log('asd')
-
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
