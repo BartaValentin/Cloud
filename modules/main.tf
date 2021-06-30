54aea8abd229b39661ef17441b202991066e6662
Barta Valentin <bartavalentin99@gmail.com>
2021-06-30 14:39:55 +0200

    rename key vault

--- modules/main.tf
+++ modules/main.tf
@@ -22,7 +22,7 @@ data "azurerm_resource_group" "this" {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "this" {
-  name                       = "valentinkeyvault2"
+  name                       = "bartavalentinkeyvault"
   location                   = data.azurerm_resource_group.this.location
   resource_group_name        = data.azurerm_resource_group.this.name
   tenant_id                  = data.azurerm_client_config.current.tenant_id
